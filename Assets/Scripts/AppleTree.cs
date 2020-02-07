d0a1bd4d84dfcff801685d8ac881c154781fc539
antsanti7 <60456844+antsanti7@users.noreply.github.com>
2020-02-06 21:52:33 -0500

    Final Push

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -13,14 +13,13 @@ public class AppleTree : MonoBehaviour
 
 	void Start()
 	{
-		//	Dropping	apples	every	second
-		Invoke("DropApple",2f);                                                                                                                                                        //	a
+		Invoke("DropApple",2f);                                                                                                                                                        
 	}
 	void DropApple()
-	{                                                                                                                                                                                                       //	b
-		GameObject apple = Instantiate<GameObject>(applePrefab);                      //	c
-		apple.transform.position =transform.position;                                                                     //	d
-		Invoke("DropApple", secondsBetweenAppleDrops);                                                              //	e
+	{                                                                                                                                                                                                       
+		GameObject apple = Instantiate<GameObject>(applePrefab);                      
+		apple.transform.position =transform.position;                                                                     
+		Invoke("DropApple", secondsBetweenAppleDrops);                                                          
 	}
 
 
