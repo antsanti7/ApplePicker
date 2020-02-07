88b1018a5e6a34c520daba4049522fe7ce8cfc4b
antsanti7 <60456844+antsanti7@users.noreply.github.com>
2020-02-06 21:14:17 -0500

    Basket movement

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Update()
+	{
+		Vector3 mousePos2D =Input.mousePosition;                                                                                                                    
+																																		
+		mousePos2D.z = -Camera.main.transform.position.z; 
+	
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+	}
 }
