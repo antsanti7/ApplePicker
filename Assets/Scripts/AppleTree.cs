236da2f459b9cbba6c1ec33fa6a6f75e42316f68
antsanti7 <60456844+antsanti7@users.noreply.github.com>
2020-02-06 20:50:00 -0500

    Apple Script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Set	in	Inspector")]
+	public GameObject applePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 20f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		//	Dropping	apples	every	second
+	}
+
+	void Update()
+	{
+		Vector3 pos = transform.position;                                                                   
+		pos.x += speed * Time.deltaTime;                                                                       
+		transform.position = pos;
+
+		if (pos.x < -leftAndRightEdge)
+		{
+			speed = Mathf.Abs(speed);
+		}
+		else if (pos.x > leftAndRightEdge)
+		{
+			speed = -Mathf.Abs(speed);
+		}
+	}
+
+	void FixedUpdate() 
+	{
+		if (Random.value < chanceToChangeDirections) 
+		{
+			speed *= -1;
+		}
+	}
 }
+
